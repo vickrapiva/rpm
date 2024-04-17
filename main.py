3b0a2318e56566cb1a4eed6fe0ec462dcf08d587
kashunya <156766930+kashunya@users.noreply.github.com>
2024-04-17 16:27:17 +0300

    код бота

---
+++ main.py
@@ -0,0 +1,26 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher, Router
+from aiogram.types import Message
+from aiogram.filters import Command 
+
+bot = Bot(token="7086038652:AAEKbcrL_Mod-9KNUf2jZGNBE_ZYj7ti0jg")
+dp = Dispatcher()
+
+router = Router()
+
+@router.message(Command("start"))
+async def start_handler(msg: Message):
+    await msg.answer(text="Привет, я бот, который сообщит тебе, какой сегодня праздник")
+    await bot.set_my_commands([
+        BotCommand
+    ])
+
+
+async def main():
+    await dp.start_polling(bot)
+
+dp.include_routers(router)
+
+if __name__ == '__main__':
+    asyncio.run(main())
